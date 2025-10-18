44dcfa4bd60d1871608c00318b900ab264f31f38
kasia <73126@student.wsiz.edu.pl>
2025-10-18 21:25:02 +0200

    homework/LAB-1 - zadanie 7 - program

---
+++ src/laboratorium_1/additional/task_7/task_7.py
@@ -0,0 +1,29 @@
+
+def podaj_a():
+    try:
+        liczba_a = float(input(f"Podaj liczbę rzeczywistą a:"))
+        return liczba_a
+    except ValueError:
+        print("Proszę podać poprawnie liczbę rzeczywistą")
+        return podaj_a()
+
+
+def podaj_b():
+    try:
+        liczba_b = float(input(f"Podaj liczbę rzeczywistą b:"))
+        return liczba_b
+    except ValueError:
+        print("Proszę podać poprawnie liczbę rzeczywistą")
+        return podaj_b()
+
+x = podaj_a()
+y = podaj_b()
+
+def rozwiaz_rownanie(a,b):
+    if a==0:
+        if b==0:
+            print(f"Dla a równego {a}, oraz b równego {b}, występuje nieskończenie wiele rozwiązań")
+        else: print(f"Dla a równego {a}, oraz b równego {b}, nie ma rozwiązania - sprzeczność")
+    else: print(f"Dla a równego {a}, oraz b równego {b}, x={-b/a}")
+
+rozwiaz_rownanie(x,y)
