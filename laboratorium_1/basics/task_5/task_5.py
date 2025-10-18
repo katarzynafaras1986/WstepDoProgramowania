b03d0fe8067e2983b678822bb4a4b28e237a932d
kasia <73126@student.wsiz.edu.pl>
2025-10-18 15:26:52 +0200

    homework/LAB-1 - zadanie 5

---
+++ laboratorium_1/basics/task_5/task_5.py
@@ -0,0 +1,33 @@
+def zwaliduj(a):
+    if a<=0:
+        print("Proszę podać poprawną długośc dla boku a")
+        return False
+    return True
+
+
+def podaj_a():
+    try:
+        bok_a = int(input(f"Podaj długośc boku a prostokąta:"))
+        return bok_a if zwaliduj(bok_a) else podaj_a()
+    except ValueError:
+        print("Proszę podać poprawną długośc dla boku a")
+        podaj_a()
+
+
+def podaj_b():
+    try:
+        bok_b = int(input(f"Podaj długośc boku b prostokąta:"))
+        return bok_b if zwaliduj(bok_b) else podaj_b()
+    except ValueError:
+        print("Proszę podać poprawną długość  dla boku b")
+        podaj_b()
+
+
+def oblicz(a, b):
+    print(f"Pole prostokąta o bokach {a} oraz {b} wynosi {a * b}")
+    print(f"Obwód prostokąta o bokach {a} oraz {b} wynosi {2 * (a + b)}")
+
+
+x = podaj_a()
+y = podaj_b()
+oblicz(x, y)
