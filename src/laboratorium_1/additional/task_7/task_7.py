cf3fdf8e9be152c227414ad1413a6d7f16cfde53
kasia <73126@student.wsiz.edu.pl>
2025-10-22 19:59:38 +0200

    homework/LAB-1 - zadanie 7 - 8

--- src/laboratorium_1/additional/task_7/task_7.py
+++ src/laboratorium_1/additional/task_7/task_7.py
@@ -1,29 +1,33 @@
-
 def podaj_a():
     try:
-        liczba_a = float(input(f"Podaj liczbę rzeczywistą a:"))
+        liczba_a = float(input(f"Podaj liczbę wymierną a w formacie dziesiętnym:"))
         return liczba_a
     except ValueError:
-        print("Proszę podać poprawnie liczbę rzeczywistą")
+        print("Proszę podać poprawnie liczbę wymierną w formacie dziesiętnym")
         return podaj_a()
 
 
 def podaj_b():
     try:
-        liczba_b = float(input(f"Podaj liczbę rzeczywistą b:"))
+        liczba_b = float(input(f"Podaj liczbę wymierną b w formacie dziesiętnym:"))
         return liczba_b
     except ValueError:
-        print("Proszę podać poprawnie liczbę rzeczywistą")
+        print("Proszę podać poprawnie liczbę wymierną w formacie dziesiętnym")
         return podaj_b()
+
 
 x = podaj_a()
 y = podaj_b()
 
-def rozwiaz_rownanie(a,b):
-    if a==0:
-        if b==0:
-            print(f"Dla a równego {a}, oraz b równego {b}, występuje nieskończenie wiele rozwiązań")
-        else: print(f"Dla a równego {a}, oraz b równego {b}, nie ma rozwiązania - sprzeczność")
-    else: print(f"Dla a równego {a}, oraz b równego {b}, x={-b/a}")
 
-rozwiaz_rownanie(x,y)
+def rozwiaz_rownanie(a, b):
+    if a == 0:
+        if b == 0:
+            print(f"Dla a równego {a}, oraz b równego {b}, występuje nieskończenie wiele rozwiązań")
+        else:
+            print(f"Dla a równego {a}, oraz b równego {b}, nie ma rozwiązania - sprzeczność")
+    else:
+        print(f"Dla a równego {a}, oraz b równego {b}, x~{-b / a}")
+
+
+rozwiaz_rownanie(x, y)
---
+++ src/laboratorium_1/additional/task_8/task_8.py
@@ -0,0 +1,54 @@
+from math import sqrt
+
+
+def zwaliduj_a(a):
+    if a == 0:
+        print(f"To nie jest równanie kwadratowe, podaj liczbę różną od zera")
+        return False
+    return True
+
+
+def podaj_a():
+    try:
+        liczba_a = float(input(f"Podaj liczbę wymierną a w formacie dziesiętnym:"))
+        return liczba_a if zwaliduj_a(liczba_a) else podaj_a()
+    except ValueError:
+        print("Proszę podać poprawnie liczbę wymierną w formacie dziesiętnym")
+        return podaj_a()
+
+
+def podaj_b():
+    try:
+        liczba_b = float(input(f"Podaj liczbę wymierną b w formacie dziesiętnym:"))
+        return liczba_b
+    except ValueError:
+        print("Proszę podać poprawnie liczbę wymierną w formacie dziesiętnym")
+        return podaj_b()
+
+
+def podaj_c():
+    try:
+        liczba_c = float(input(f"Podaj liczbę wymierną c w formacie dziesiętnym:"))
+        return liczba_c
+    except ValueError:
+        print("Proszę podać poprawnie liczbę wymierną w formacie dziesiętnym")
+        return podaj_c()
+
+
+x = podaj_a()
+y = podaj_b()
+z = podaj_c()
+
+
+def rozwiaz_rownanie(a, b, c):
+    delta = b ** 2 - 4 * a * c
+    if delta == 0:
+        print(f"Dla a równego {a}, b równego {b} oraz c ={c}, delta jest równa {delta}, x wynosi {-b / (2 * a)}")
+    elif delta > 0:
+        print(
+            f"Dla a równego {a}, b równego {b} oraz c ={c}, delta jest równa {delta}. x1 wynosi {(-b - sqrt(delta)) / (2 * a)} a x2 wynosi {(-b + sqrt(delta)) / (2 * a)}")
+    elif delta < 0:
+        print(f"Delta jest równa {delta}. Brak rozwiązań")
+
+
+rozwiaz_rownanie(x, y, z)
