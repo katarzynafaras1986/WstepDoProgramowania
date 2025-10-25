a58bc607afbd59b11bdf8f13314555f77b643cd9
Katarzyna Faraś <73136@student.wsiz.edu.pl>
2025-10-25 10:18:59 +0200

    lab2/task_1

--- src/laboratorium_2/task_1/tesk_1.py
+++ src/laboratorium_2/task_1/tesk_1.py
@@ -5,3 +5,19 @@
 # Studenci którzy otrzymali liczbę punków z przedziału 50-80, mogą poprawić jego wynik.
 # Studenci, którzy zdobyli poniżej 50 punktów, muszą go poprawić.
 
+def podaj_liczbe_punktow():
+    try:
+        liczba_a = int(input(f"Podaj liczbę zdobytych punktów:"))
+        return liczba_a
+    except ValueError:
+        print("Proszę podać poprawnie liczbę punktow")
+        return podaj_liczbe_punktow()
+
+liczba_punktow = podaj_liczbe_punktow()
+
+if liczba_punktow > 80:
+    print("Zaliczasz egzamin w terminie 0")
+elif liczba_punktow < 80 and liczba_punktow > 50:
+    print("Możesz poprawić  wynik")
+elif liczba_punktow < 50:
+    print("Musisz poprawić wynik")
