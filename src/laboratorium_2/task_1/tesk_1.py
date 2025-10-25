2acf1a61f663908110047c21f3162aa673065429
Katarzyna Faraś <73136@student.wsiz.edu.pl>
2025-10-25 10:09:16 +0200

    lab2/task_1

---
+++ src/laboratorium_2/task_1/tesk_1.py
@@ -0,0 +1,7 @@
+# Zad 1.
+# Napisz prosty program, który na podstawie podanej przez Studenta liczby zdobytych punktów,
+# poinformuje go o rezultacie egzaminu.
+# Każdy Student, który zdobył powyżej 80 punktów zalicza egzamin w terminie 0
+# Studenci którzy otrzymali liczbę punków z przedziału 50-80, mogą poprawić jego wynik.
+# Studenci, którzy zdobyli poniżej 50 punktów, muszą go poprawić.
+
