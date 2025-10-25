efa8ce75600ae81ccb17fde93bd0281c2439a7f6
Katarzyna Faraś <73136@student.wsiz.edu.pl>
2025-10-25 11:28:47 +0200

    lab2/task_7-8 puste

---
+++ src/laboratorium_2/basics/task_8/task_8.py
@@ -0,0 +1,7 @@
+# Zad 8.
+# Stwórz program, który wykorzystując operator wycinania z podanego ciąg znaków (zmienna text)
+# wyodrębni:
+# pierwsze trzy znaki
+# ostatnie dwa znaki
+# text = 'Studiuje-Informatykę'
+# Wynik wydrukuj do konsoli.
