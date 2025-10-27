1e21cb7a7fb5521080a28a09bf037a0946b45cff
kasia <73126@student.wsiz.edu.pl>
2025-10-27 19:58:19 +0100

    lab_2_task_9

--- src/laboratorium_2/additional/task_9/task_9.py
+++ src/laboratorium_2/additional/task_9/task_9.py
@@ -1,3 +1,9 @@
 # Zad 9. dodatkowe
 # Napisz skrypt zmieniający wszystkie duże litery małe i na odwrót.
 # Podpowiedz: skorzytaj z metody swapcase().
+
+def zmien_wielkosc_liter(text):
+    print(f"Text ze zmienioną wilkością liter {text.swapcase()}")
+
+
+zmien_wielkosc_liter(input(f"Wprowadź tekst do zmiany wielkości liter: "))
