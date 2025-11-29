64353a0ee42c38f9f54e891675116edb0f918fa7
Katarzyna Faraś <73136@student.wsiz.edu.pl>
2025-11-29 10:46:27 +0100

    lab_4

--- src/laboratorium_4/task_3.py
+++ src/laboratorium_4/task_3.py
@@ -7,3 +7,29 @@
 # e. Wczyta dwa łańcuchy, a w trzecim łańcuchu zapamięta połączone te dwa łańcuchy.
 # f. Wczyta dwa łańcuchy, a w trzecim łańcuchu zapamięta pierwszą połowę pierwszego i drugą połowę
 # drugiego
+
+imie = input("Podaj imie: ")
+
+print("Witaj " + imie)
+
+wiek = input("Podaj wiek: ")
+
+print("Twój wiek to: " + wiek)
+
+imie = input("Podaj imie: ")
+nazwisko = input("Podaj nazwisko: ")
+
+print("Twoje inicjały: ", imie[0:1].upper(), nazwisko[0:1].upper())
+
+lancuch = input("Podaj lancuch1: ")
+lancuch2 = input("Podaj lancuch2: ")
+
+print("5 razy łańcuch: ", lancuch * 5)
+
+lancuch = input("Podaj lancuch1: ")
+lancuch2 = input("Podaj lancuch2: ")
+
+print("Połączone łańcuchy 1 i 2: ", lancuch + lancuch2)
+
+print("Połączone połówki: ", lancuch[:len(lancuch) // 2] + lancuch2[:len(lancuch2) // 2])
+
