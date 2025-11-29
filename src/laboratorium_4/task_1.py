3ace08b809322491805b9c1fa3115f1744a3c7d6
Katarzyna Faraś <73136@student.wsiz.edu.pl>
2025-11-29 10:32:31 +0100

    lab_4

--- src/laboratorium_4/task_1.py
+++ src/laboratorium_4/task_1.py
@@ -1,3 +1,27 @@
 # Zad. 1
 # Utwórz listę: Moja_lista=[1, 17, 3, 5, 3, 4, 86, 90, 2, 21], przetestuj działanie wszystkich
 # zaprezentowanych operacji na listach, a ich wynik wyświetl w konsoli.
+
+moja_lista = [1, 17, 3, 5, 3, 4, 86, 90, 2, 21]
+moja_lista2 = [181, 66, 36]
+
+print('Pierwszy element: ', moja_lista[0])
+print('Ostatni element: ', moja_lista[-1])
+print('Długośc listy: ', len(moja_lista))
+print('Największa wartość: ', max(moja_lista))
+print('Najmniejsza wartość: ', min(moja_lista))
+print('Posortowana: ', sorted(moja_lista))
+moja_lista.append(40)
+print('Z dodanym elementem: ', moja_lista)
+print('Z dodanym elementem w indexie 5: ', moja_lista.insert(5, 50))
+print('Ostatni zwrócony element: ', moja_lista.pop())
+moja_lista.reverse()
+print('Odwrócona lista: ', moja_lista)
+moja_lista3 = moja_lista + moja_lista2
+print('Połączona lista 2 i 3: ', moja_lista3)
+moja_lista4 = moja_lista + moja_lista2
+print('Połączenie listy 1 i 2 pięciokrotnie: ', moja_lista4 * 5)
+print('Od początku do elementu 5: ', moja_lista[:5])
+print('Od elementu o inexie 5 do końca: ', moja_lista[5:])
+print('Zakres elementów 2-8 z krokiem 2: ', moja_lista[2:8:2])
+print('Odwrócenie listy: ', moja_lista[::-1])
--- src/laboratorium_4/task_2.py
+++ src/laboratorium_4/task_2.py
@@ -3,3 +3,12 @@
 # b. Dodaj na końcu dwie osoby i pobierz ostatnią z nich poleceniem pop().
 # c. Na pozycji 3 dodaj jeszcze jedną osobę.
 # d. Odwróć kolejność na liście i zduplikuj ją.
+lista_z_imionami = ["Kasia", "Ewelina", "Kuba", "Krzysio", "Bartuś"]
+lista_z_imionami.sort()
+print("Lista posortowana: ", lista_z_imionami)
+lista_z_imionami.append("Marcin")
+lista_z_imionami.append("Piotrek")
+print("Ostatnia osoba popem: ", lista_z_imionami.pop())
+lista_z_imionami.reverse()
+print("Odwrócona: ", lista_z_imionami)
+print("Zduplikowana: ", lista_z_imionami*2)
