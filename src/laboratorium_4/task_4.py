15d7b165a914dc2c68f93cce2ec50a0cbfd55fde
Katarzyna Faraś <73136@student.wsiz.edu.pl>
2025-11-29 11:16:17 +0100

    lab_4

--- src/laboratorium_4/task_4.py
+++ src/laboratorium_4/task_4.py
@@ -1,15 +1,25 @@
 # Zad. 4 dodatkowe
 # Napisz program, który wczyta od użytkownika zdanie. Wypisz z niego wszystkie litery w kolejności
-# alfabetycznej, a następnie wypisze których litera alfabetu nie zawiera.
-# Krotka (ang. tuple) w Pythonie to struktura danych, która jest uporządkowaną sekwencją elementów.
-# Krotki są podobne do list, ale mają kilka kluczowych różnic:
-# Cechy krotek:
-# 1. Niezmienność - po utworzeniu krotki nie można zmieniać, dodawać ani usuwać jej
-# elementów. Oznacza to, że krotka jest niezmienialna, co odróżnia ją od listy, która jest
-# zmienialna.
-# 2. Uporządkowanie - elementy krotki mają swoje miejsce, czyli każdemu elementowi
-# przypisany jest indeks (numer porządkowy), który zaczyna się od 0.
-# 3. Mogą zawierać różne typy danych - w jednej krotce można przechowywać elementy
-# różnych typów, np. liczby, ciągi znaków, inne krotki itp.
-# 4. Zastosowanie nawiasów okrągłych - krotki są definiowane przy użyciu nawiasów okrągłych
-# (), podczas gdy listy są definiowane przy użyciu nawiasów kwadratowych [].
+from traceback import format_tb
+
+zdanie = input("Podaj zdanie: ")
+
+zdanie_jako_lista = list(zdanie)
+
+zdanie_jako_lista = [element for element in zdanie_jako_lista if not element.isdigit() and len(element.strip()) != 0]
+
+print("Lista bez cyfr: ", zdanie_jako_lista)
+
+print(zdanie_jako_lista)
+
+zdanie_jako_lista.sort()
+
+litery_alfabetu =  set(zdanie_jako_lista)
+
+print("Litery: ", litery_alfabetu)
+
+alfabet = {"a", "b", "c", "d", "e", "f", "g", "h", 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
+
+nowa_lista = [element for element in alfabet if element not in litery_alfabetu]
+
+print("Alfabet bez liter użytych w zdaniu: ", nowa_lista)
--- src/laboratorium_4/task_5.py
+++ src/laboratorium_4/task_5.py
@@ -1,2 +1,3 @@
 # Zad 5.
 # Utwórz krotkę zwierająca dni tygodnia.
+krotka = ("Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela")
