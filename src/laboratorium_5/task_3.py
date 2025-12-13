d85b7b975cd4881a56d85a7c6de8afd7baebd0ef
Katarzyna Faraś <73136@student.wsiz.edu.pl>
2025-12-13 13:13:39 +0100

    lab_5_task_3_5

--- src/laboratorium_5/task_3.py
+++ src/laboratorium_5/task_3.py
@@ -3,7 +3,7 @@
 # argumentów. Wykorzystaj funkcję filter oraz lambda.
 
 def wypisz_tylko_liczby_parzyste(*args):
-    return  list(filter(lambda x: x % 2 == 0, list(filter(lambda x: isinstance(x, int), args))))
+    return  list(filter(lambda x: x % 2 == 0, list(filter(lambda x: isinstance(x, int) or isinstance(x, float), args))))
 
 print(wypisz_tylko_liczby_parzyste(1,2,3,4,5))
 print(wypisz_tylko_liczby_parzyste(1,2,3,4,"cos"))
--- src/laboratorium_5/task_5.py
+++ src/laboratorium_5/task_5.py
@@ -9,3 +9,14 @@
 # b. TypeError, gdy argumenty mają niewłaściwe typy
 # c. IndexError, gdy indeksy wychodzą poza zakres
 # 5. (Dla chętnych – napisanie testów przy pomocy biblioteki pytest)
+def zamien_miejscami_elementy_listy(lista, inex1, index2):
+    if len(lista) == 0:
+        raise ValueError("Lista nie może być pusta")
+    if isinstance(inex1, int):
+        raise TypeError("Index1 musi być intem")
+    if isinstance(index2, int):
+        raise TypeError("Index2 musi być intem")
+    buffer = lista[inex1]
+    lista[inex1] = lista[index2]
+    lista[index2] = buffer
+    if index1 len(lista) == 0:
