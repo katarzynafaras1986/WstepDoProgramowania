03d43b2bce6d8e0583fd1dff005aa5026698d958
Katarzyna Faraś <73136@student.wsiz.edu.pl>
2025-12-13 12:46:17 +0100

    lab_5_task_2

--- src/laboratorium_5/task_3.py
+++ src/laboratorium_5/task_3.py
@@ -1,3 +1,12 @@
 # Zadanie 3.
 # Napisz funkcję, która zwraca listę tylko z liczbami parzystymi. Funkcja przyjmuje dowolną ilość
 # argumentów. Wykorzystaj funkcję filter oraz lambda.
+
+def wypisz_tylko_liczby_parzyste(*args):
+    for arg in args:
+        if not isinstance(arg, int):
+            raise TypeError("Parametr musi być liczbą.")
+    return  list(filter(lambda x: x % 2 == 0, args))
+
+print(wypisz_tylko_liczby_parzyste(1,2,3,4,5))
+print(wypisz_tylko_liczby_parzyste(1,2,3,4,"cos"))
