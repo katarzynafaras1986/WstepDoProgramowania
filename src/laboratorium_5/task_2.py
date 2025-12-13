2957d41e2fce790d145c03007596ab1650ea09bb
Katarzyna Faraś <73136@student.wsiz.edu.pl>
2025-12-13 12:38:06 +0100

    lab_5_task_2

--- src/laboratorium_5/task_2.py
+++ src/laboratorium_5/task_2.py
@@ -2,3 +2,19 @@
 # Napisz funkcję, która będzie tworzyć profil użytkownika z obowiązkowym imieniem oraz adresem
 # email i dowolnymi dodatkowymi informacjami (min. 3). Funkcja zwraca słownik zawierający
 # wszystkie dane profilu. Wykorzystaj funkcję isinstance() oraz strip().
+
+def utworz_profil_uzytkownika(**kwargs):
+    if 'email' not in kwargs or 'imie' not in kwargs:
+        raise ValueError("Musisz podać obowiązkowe argumenty: 'email' i 'imie'.")
+    imie_raw = kwargs['imie'].strip()
+    email_raw = kwargs['email'].strip()
+
+    if not isinstance(imie_raw, str):
+        raise TypeError("imie must be a string")
+    if not isinstance(email_raw, str):
+        raise TypeError("email must be a string")
+
+    return kwargs
+
+
+print(utworz_profil_uzytkownika(imie="kasia", email="k@o2.pl", wiek=39))
