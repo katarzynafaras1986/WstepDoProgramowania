aafb6d9796bd505ccdb43c78216fbbb08f7fde15
Katarzyna Faraś <73136@student.wsiz.edu.pl>
2025-12-13 13:03:46 +0100

    lab_5_task_3_4

--- src/laboratorium_5/task_3.py
+++ src/laboratorium_5/task_3.py
@@ -3,10 +3,7 @@
 # argumentów. Wykorzystaj funkcję filter oraz lambda.
 
 def wypisz_tylko_liczby_parzyste(*args):
-    for arg in args:
-        if not isinstance(arg, int):
-            raise TypeError("Parametr musi być liczbą.")
-    return  list(filter(lambda x: x % 2 == 0, args))
+    return  list(filter(lambda x: x % 2 == 0, list(filter(lambda x: isinstance(x, int), args))))
 
 print(wypisz_tylko_liczby_parzyste(1,2,3,4,5))
 print(wypisz_tylko_liczby_parzyste(1,2,3,4,"cos"))
--- src/laboratorium_5/task_4.py
+++ src/laboratorium_5/task_4.py
@@ -1,3 +1,11 @@
 # Zadanie 4.
 # Napisz funkcję sum_digits(n), która oblicza sumę cyfr liczby rekurencyjnie. Napisz dokumentacje tej
 # funkcji, która opisywać będzie jej działanie.
+
+def sum_digits(n):
+    if n < 10:
+        return n
+    else:
+        return n % 10 + sum_digits(n//10)
+
+print(sum_digits(523))
