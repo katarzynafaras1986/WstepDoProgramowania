381363772dc60652e903b97a0e8c6b58e7f1596e
kasia <73126@student.wsiz.edu.pl>
2026-01-23 11:39:15 +0100

    lab6_task6

--- src/laboratorium_6/task_5_6.py
+++ src/laboratorium_6/task_5_6.py
@@ -9,3 +9,13 @@ df['Marża'] = (df['Sprzedaż'] - df['Koszt']) / df['Sprzedaż']
 df_posortowane = df.sort_values(by=['Rok', 'Kraj'], ascending=True)
 
 print(df_posortowane)
+
+# Zadanie 2. Zwróć rekordy dla DE w latach >= 2023 i tylko kolumny sprzedaż i marża.
+
+wynik = df_posortowane.loc[
+    (df_posortowane['Kraj'] == 'DE') & (df_posortowane['Rok'] >= 2023),
+    ['Sprzedaż', 'Marża']
+]
+
+# Wyświetlenie wyników dla roku 2026 i lat ubiegłych
+print(wynik)
